e177dd294fe116da5f2fb0fa4c40174409d29902
Vitaliy <vitaliy@prisbank.com>
2018-02-22 16:15:01 +0200

    add person

--- src/Person.js
+++ src/Person.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import './Person.css'
 
-class Person extends Component{
+class Person extends Component {
     render(){
         return (
-            <div>{this.props.data.last_name}</div>
+            <p><span className="Person">{this.props.data.last_name} {this.props.data.first_name} {this.props.data.patronymic_name}</span></p>
         )
     }
 }

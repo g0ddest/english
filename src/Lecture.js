412a2ff8bbd33bf85caa5e24ed3cefcf81c4133f
Vitaliy <vitaliy@prisbank.com>
2018-02-22 09:14:42 +0200

    add lecture number

--- src/Lecture.js
+++ src/Lecture.js
@@ -3,11 +3,19 @@ import './Lecture.css';
 
 class Lecture extends Component{
 
+    lecture_num(){
+        if(this.props.data.number.length === 1)
+            return this.props.data.number[0] + ". ";
+        if(this.props.data.number.length > 1)
+            return this.props.data.number[0] + "–" + this.props.data.number[this.props.data.number.length - 1] + ". ";
+        return "";
+    }
+
     render(){
         let data = this.props.data;
         return (
             <div class="Lecture">
-                <h2>{data.name}</h2>
+                <h2><span>{this.lecture_num()}</span>{data.name}</h2>
             </div>
         );
     }
